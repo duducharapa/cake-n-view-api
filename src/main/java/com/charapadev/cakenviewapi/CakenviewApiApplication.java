a7085c08efc4e8c7b1059cd0bd7bd1b4a7b727d5
Eduardo Charapa <charapadev@gmail.com>
2023-10-13 15:19:03 -0300

    first: created initial template

---
+++ src/main/java/com/charapadev/cakenviewapi/CakenviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.charapadev.cakenviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakenviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakenviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charapadev/cakenviewapi/CakenviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charapadev.cakenviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakenviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
