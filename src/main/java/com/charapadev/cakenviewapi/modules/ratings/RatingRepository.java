2a19b2f3c0f600c3e202bd0f0c6d54c6ca63339b
Eduardo Charapa <charapadev@gmail.com>
2023-10-22 03:59:23 -0300

    fix: users can rate an cake only one time

--- src/main/java/com/charapadev/cakenviewapi/modules/ratings/RatingRepository.java
+++ src/main/java/com/charapadev/cakenviewapi/modules/ratings/RatingRepository.java
@@ -1,6 +1,7 @@
 package com.charapadev.cakenviewapi.modules.ratings;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,10 +10,13 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
+    @Query("SELECT r FROM Rating r JOIN r.cake c JOIN r.user u WHERE c.id = :cakeId AND u.id = :userId")
+    Optional<Rating> findByCakeAndUser(Long cakeId, Long userId);
+
     @Query("SELECT r FROM Rating r JOIN r.cake c WHERE c.id = :cakeId")
     Page<Rating> findAllByCake(Long cakeId, Pageable pageable);
 
     @Query("SELECT avg(r.number) FROM Rating r JOIN r.cake c WHERE c.id = :cakeId")
     BigDecimal getAverageRatingFromCake(Long cakeId);
-    
+
 }
--- src/main/java/com/charapadev/cakenviewapi/modules/ratings/RatingService.java
+++ src/main/java/com/charapadev/cakenviewapi/modules/ratings/RatingService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.charapadev.cakenviewapi.exceptions.UnprocessableEntityException;
 import com.charapadev.cakenviewapi.modules.cakes.dtos.UpdateCakeDTO;
 import com.charapadev.cakenviewapi.modules.cakes.entities.Cake;
 import com.charapadev.cakenviewapi.modules.cakes.services.CakeService;
@@ -29,6 +30,10 @@ public class RatingService {
 
     @Transactional
     public void add(Cake cake, User user, RateCakeDTO rateDTO) {
+        boolean alreadyRatedByUser = ratingRepository.findByCakeAndUser(cake.getId(), user.getId())
+            .isPresent();
+        if (alreadyRatedByUser) throw new UnprocessableEntityException("This user already rated this cake");
+
         Rating newRating = Rating.builder()
             .cake(cake)
             .user(user)
